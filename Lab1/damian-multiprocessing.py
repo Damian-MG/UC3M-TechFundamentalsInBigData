aec722320c4eb9d8f76bcfa1e8a4494eace16198
Damian-MG <malenogon_f@hotmail.com>
2022-10-09 21:56:51 +0200

    multiprocessing definitivo

--- Lab1/damian-multiprocessing.py
+++ Lab1/damian-multiprocessing.py
@@ -1,8 +1,8 @@
 __author__      = "DAMIAN Maleno, NEREA Izcue, PABLO Alonso "
 __credits__     = ["Damian Maleno", "Nerea Izcue", "Pablo Alonso"]
 __version__     = "1.0"
-__email__       = ["100484916@alumnos.uc3m.es",]
-__status__      = "In dev"
+__email__       = ["100484916@alumnos.uc3m.es","100492040@alumnos.uc3m.es","100483840@alumnos.uc3m.es"]
+__status__      = "Finished"
 
 """
 Program to count the matches of a pattern introduced using the keyboard against all the proteins in the dataset using
@@ -26,7 +26,6 @@ import multiprocessing as mp
 from collections import ChainMap
 import subprocess as sp
 
-
 file =  "proteins.csv"
 
 def getLength(file):
@@ -36,7 +35,7 @@ def getSequence():
     check = False
     while check == False:
         sequence = input("Input the pattern to search matching the regex ^[A-D]+$ :\n").upper()
-        if re.match(r"^[A-D]+$",sequence):
+        if re.match(r"^[A-Z]+$",sequence):
             check = True
     return(sequence)
 
@@ -57,17 +56,18 @@ def countMatches(Start, Stop, Pattern, File):
 
 if __name__ == '__main__':
     sequence = getSequence()
-    iterdata = chunking(Lines= getLength(file)) # si lo quieres correr en windows quita el getLength y pon un entero con el numero de lineas
+    iterdata = chunking(Lines= getLength(file))
     iterdata = [(start, stop, sequence, file) for start, stop in iterdata.items()]
     t_stamp = time.time()
     pool = mp.Pool(mp.cpu_count())
-    results = pool.starmap(countMatches, iterdata) # probar tbm el asincrono a ver que pasa
+    results = pool.starmap_async(countMatches, iterdata)
+    hits = results.get()
     pool.close()
     print("Elapsed execution time: ",time.time() - t_stamp," s")
-    hits = dict(ChainMap(*results))
+    hits = dict(ChainMap(*hits))
     hits = {k: v for k,v in sorted(hits.items(), key=itemgetter(1), reverse=True)}
     print({k: hits[k] for k in list(hits)[:5]})
     hits_10 = dict(islice(hits.items(),10))
-    #plt.bar([ str(i) for i in hits_10.keys()], hits_10.values(), color='g')
-    #plt.show()
+    plt.bar([ str(i) for i in hits_10.keys()], hits_10.values(), color='g')
+    plt.show()
    
