058ecf049de8b7f6366fcbce96779f42321602c6
Damian-MG <malenogon_f@hotmail.com>
2022-10-09 20:42:42 +0200

    definitivo threading

--- Lab1/damian-threading.py
+++ Lab1/damian-threading.py
@@ -1,8 +1,8 @@
 __author__      = "DAMIAN Maleno, NEREA Izcue, PABLO Alonso "
 __credits__     = ["Damian Maleno", "Nerea Izcue", "Pablo Alonso"]
 __version__     = "1.0"
-__email__       = ["100484916@alumnos.uc3m.es",]
-__status__      = "In dev"
+__email__       = ["100484916@alumnos.uc3m.es","100492040@alumnos.uc3m.es","100483840@alumnos.uc3m.es"]
+__status__      = "Finished"
 
 """
 Program to count the matches of a pattern introduced using the keyboard against all the proteins in the dataset using
@@ -16,11 +16,6 @@ Threading:
 - One GIL (Global Interpreter Lock for all threads)
 """
 
-# Vamos a tener que hacer dos implementaciones del threading
-# La primera cada thread hace como en el multiproceso y se gestiona su propia apertura de datos
-# La segunda comparten entre ellas la apertura del fichero, el único problema que podemos encontrar es que 
-# haya concurrencia la I/O con el fichero por lo que un thread bloquee al otro
-
 from itertools import islice
 from operator import itemgetter
 import time
@@ -32,7 +27,6 @@ import multiprocessing as mp
 from collections import ChainMap
 import subprocess as sp
 
-
 file =  "proteins.csv"
 
 def getLength(file):
@@ -42,14 +36,14 @@ def getSequence():
     check = False
     while check == False:
         sequence = input("Input the pattern to search matching the regex ^[A-D]+$ :\n").upper()
-        if re.match(r"^[A-D]+$",sequence):
+        if re.match(r"^[A-Z]+$",sequence):
             check = True
     return(sequence)
 
 def chunking(Lines, Num_threads):
     iterdata = {}
     cpus = mp.cpu_count()
-    chunk_size = int(Lines/mp.cpu_count())
+    chunk_size = int(Lines/Num_threads)
     for i in range (cpus): iterdata[i*chunk_size+1] = (i+1)*chunk_size+1
     return iterdata
 
@@ -61,16 +55,16 @@ def countThread(Start, Stop, Pattern, File):
             occurrences[int(row[0])] = row[1].count(Pattern)
     results.append(occurrences)
 
-
+"""
+If your threads don't do I/O, synchronization, etc., and there's nothing else running, 1 thread per core will get you the
+best performance. However that very likely not the case. Adding more threads usually helps, but after some point, they
+cause some performance degradation.
+"""
 if __name__ == '__main__':
     sequence = getSequence()
-    # If your threads don't do I/O, synchronization, etc., and there's nothing else running, 1 thread per core will get you the
-    # best performance. However that very likely not the case. Adding more threads usually helps, but after some point, they
-    # cause some performance degradation.
-    num_threads = mp.cpu_count()
-    #iterdata = chunking(Lines= getLength(file), Num_threads= num_threads) # para hacer las pruebas de optimización, cambiar num_threads por un entero no mayor que mp.cpu_count()
+    num_threads = 5 # optimal thread number found for 500.000 lines
+    iterdata = chunking(Lines= getLength(file), Num_threads= num_threads))
     results = []
-    iterdata = chunking(Lines= 50000, Num_threads= num_threads) # para hacer las pruebas de optimización, cambiar num_threads por un entero no mayor que mp.cpu_count()
     iterdata = [(start, stop, sequence, file) for start, stop in iterdata.items()]
     ths = []
     t_stamp = time.time()
@@ -78,10 +72,8 @@ if __name__ == '__main__':
          th = threading.Thread(name='th%s' %(i+1), target= countThread, args= iterdata[i])   
          th.start()
          ths.append(th)
-
     for thread in ths:
          th.join()
-
     print("Elapsed execution time: ",time.time() - t_stamp," s")
     hits = dict(ChainMap(*results))
     hits = {k: v for k,v in sorted(hits.items(), key=itemgetter(1), reverse=True)}
